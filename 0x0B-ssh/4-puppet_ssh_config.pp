0d82a7a926c44c7368fa7a399e91f0be979fccb2
Daransoto <901@holbertonschool.com>
2019-12-10 15:56:15 +0000

    Fixed puppet style

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 #Changes the configuration of the ssh client.
 include stdlib
+file_line {'Turn off passwd auth':
+ensure  => present,
+path    => '/etc/ssh/sshd_config',
+line    => 'PasswordAuthentication no',
+match   => '^PasswordAuthentication',
+replace => true
+}
 file_line {'Declare identity file':
 ensure => present,
 path   => '/etc/ssh/sshd_config',
 line   => 'IdentityFile ~/.ssh/holberton',
-}
-file_line {'Turn off passwd auth':
-ensure => present,
-path   => '/etc/ssh/sshd_config',
-line   => 'PasswordAuthentication no',
-match  => '^PasswordAuthentication',
-replace => true
 }
