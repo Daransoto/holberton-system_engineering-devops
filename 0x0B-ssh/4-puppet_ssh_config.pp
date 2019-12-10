9611b8f3e7cc6d28d9bd178dc8dadbac6d0badca
Daransoto <901@holbertonschool.com>
2019-12-10 16:03:20 +0000

    Changes to ssh_config instead of sshd_config

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
 #Changes the configuration of the ssh client.
 include stdlib
 file_line {'Turn off passwd auth':
-ensure  => present,
-path    => '/etc/ssh/sshd_config',
-line    => 'PasswordAuthentication no',
-match   => '^PasswordAuthentication',
-replace => true
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no',
+match  => '^PasswordAuthentication'
 }
 file_line {'Declare identity file':
 ensure => present,
-path   => '/etc/ssh/sshd_config',
+path   => '/etc/ssh/ssh_config',
 line   => 'IdentityFile ~/.ssh/holberton',
 }
