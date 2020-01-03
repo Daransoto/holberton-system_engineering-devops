e170e41441f9d544c4099ffc59e434bc48ef3230
Daransoto <901@holbertonschool.com>
2020-01-03 04:39:51 +0000

    Included stdlib

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,10 +1,11 @@
+include stdlib
 package { 'nginx':
   ensure => installed
 }
 file_line {'header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
+  after  => 'server {',
   line   => 'add_header X-Served-By $hostname;'
 }
 service {'nginx':
