38b2da6cb4fb7ee25650b0ea12683df6f82934f6
Daransoto <901@holbertonschool.com>
2020-02-03 17:23:18 +0000

    Using get in dictionaries

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+""" For an employee ID, returns information about the TODO list progress. """
 import json
 import requests
 import sys
-""" For an employee ID, returns information about the TODO list progress. """
 
 
 if __name__ == "__main__":
     response = requests.get("https://jsonplaceholder.typicode.com/users/" +
                             sys.argv[1])
     dicti = json.loads(response.text)
-    name = dicti['name']
+    name = dicti.get('name')
     response = requests.get("https://jsonplaceholder.typicode.com/todos/" +
                             "?userId=" + sys.argv[1])
     todos = json.loads(response.text)
     tasks = len(todos)
-    completed = [task for task in todos if task['completed']]
+    completed = [task for task in todos if task.get('completed')]
     done = len(completed)
     print("Employee {} is done with tasks({}/{}):".format(name, done, tasks))
     for task in completed:
-        print("\t", task['title'])
+        print("\t", task.get('title'))
